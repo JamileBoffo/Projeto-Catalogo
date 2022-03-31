8b4227393e1a6ad0d194a3df8070c507275cb045
Jamile Boffo <97992778+JamileBoffo@users.noreply.github.com>
2022-03-31 20:15:52 -0300

    Create livro.js

---
+++ src/model/livro.js
@@ -0,0 +1,36 @@
+import Sequelize from 'sequelize'
+import { connection } from '../database/connection.js'
+
+export const variados = connection.define('variados', {
+    id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    titulo: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    ano: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    autores: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    sinopse: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    img: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
+}, {
+    freezeTableName: true,
+    createdAt: false,
+    updateAt: false,
+    timestamps: false
+})
