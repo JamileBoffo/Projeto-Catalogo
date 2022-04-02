20b49249c388a229554546d250353a63837fcb1c
Jamile Boffo <97992778+JamileBoffo@users.noreply.github.com>
2022-04-01 23:09:34 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getIndex, getId, postAdd, getAdd, getEd, postEd, getDelete, getDel } from '../controller/LivrosController.js'
+import { getIndex, getId, postAdd, getAdd, getEd, postEd, getDel } from '../controller/LivrosController.js'
 
 
 export const routes = express.Router();
@@ -9,13 +9,9 @@ routes.get('/', getIndex);
 routes.get('/detalhes/:id', getId);
 
 routes.get('/adicionar', getAdd);
-
 routes.post("/adicionar", postAdd);
 
-routes.get('/editar', getEd);
+routes.get('/editar/:id', getEd);
+routes.post('/editar/:id', postEd);
 
-routes.post('/editar', postEd);
-
-routes.get('/deletar', getDelete)
-
-routes.get('/deletar', getDel);
+routes.get('/delete/:id', getDel);
